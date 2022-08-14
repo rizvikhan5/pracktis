b051ffbaa6196f4523b0543d80ca855b9ba6a007
rizvikhan5 <mahmudulhasanrizvi47@gmail.com>
2022-08-14 23:21:25 +0600

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+document.getElementById('yes').addEventListener('click',function(){
+    document.getElementById('ok').innerText = 'Low monu biya koira loi...';
+})
+document.getElementById('No').addEventListener('click',function(){
+    document.getElementById('ok').innerText = 'Nah..Jibonaw tmre biya kortam na ';
+})
